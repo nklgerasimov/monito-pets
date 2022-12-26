5b604492756ef7e9a06ec1f9e35ba5a6293f7ebd
nklgerasimov <gniko@yandex.ru>
2022-12-26 17:08:35 +0300

    master

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ const langOptions = document.querySelector('.lang-select__options');
 langBtn.onclick = function () {
     langOptions.classList.toggle('none');
 }
+
+langOptions.onclick = function () {
+    langOptions.classList.add('none');
+    if (e.target.closest('.lang')) {
+        
+    }
+}
